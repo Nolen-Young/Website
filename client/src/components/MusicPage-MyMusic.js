8ae9cf60494767ecb7152f7e7aa1736f9d8bd10d
nyoung1 <nolen.young@wsu.edu>
2020-03-29 16:28:38 -0700

    Fixed the my music layout

--- client/src/components/MusicPage-MyMusic.js
+++ client/src/components/MusicPage-MyMusic.js
@@ -8,28 +8,32 @@ class MusicPageMyMusic extends React.Component {
 
 	render() {
 	  return (
-		<div>
+		<div id = "myMusicPage">
 			<div id = "header" className = "center">
 				<h1>Music I've Made</h1>
 		  	</div>
 			<div className = " music-players center ">
-				<table className = "my-table">
-					<tr>
-						<th>Song Title</th>
-						<th>Released Under Pseudonym</th>
-						<th>Play</th>
-					</tr>
-					<tr>
-						<td>Dark</td>
-						<td>South of Sound</td>
-						<td><ReactPlayer url = "https://soundcloud.com/southofsound/dark" height = "100px"/></td>
-					</tr>
-					<tr>
-						<td>Call Out My Name (DJ BEEF Remix)</td>
-						<td>DJ BEEF</td>
-						<td><ReactPlayer url = "https://soundcloud.com/user-243943999/the-weeknd-call-out-my-name-dj-beef-remix" height = "100px"/></td>
-					</tr>
-					
+				<table className = "music-table">
+					<thead className = " music-table-header ">
+						<tr className = "music-table-row">
+							<th className = "music-table-song-titles">Song Title</th>
+							<th className = "music-table-pseudonym">Released Under Pseudonym</th>
+							<th>Play</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr className = "music-table-row ">
+							<td className = "music-table-song-titles">Dark</td>
+							<td className = "music-table-pseudonym">South of Sound</td>
+							<td><ReactPlayer url = "https://soundcloud.com/southofsound/dark" width = "100%" height = "100px"/></td>
+						</tr>
+						<tr className = "music-table-row ">
+							<td className = "music-table-song-titles">Call Out My Name (DJ BEEF Remix)</td>
+							<td className = "music-table-pseudonym">DJ BEEF</td>
+							<td><ReactPlayer url = "https://soundcloud.com/user-243943999/the-weeknd-call-out-my-name-dj-beef-remix" width = "100%" height = "100px"/></td>
+						</tr>
+					</tbody>
+					<tfoot/>
 				</table>
 			</div>
 		</div>
