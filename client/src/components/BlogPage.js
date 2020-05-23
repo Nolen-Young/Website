61e2440f79430552c645b67808b0ef72e6ad93ac
nyoung1 <nolen.young@wsu.edu>
2020-05-23 08:27:03 -0700

    Added blog display framework.

--- client/src/components/BlogPage.js
+++ client/src/components/BlogPage.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import BlogTable from "./BlogPage-BlogTable.js"
 
 // *****************************************************
 // This class contains the code to render the Review pages.
 // ***************************************************** 
 class BlogPage extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {posts: [],
+                  postID: ""};
+}
+
   render() {
     return (
       <div className="padded-page center">
         <h1>Blog</h1>
-        <h2>This page is under construction.</h2>
+        <React.Fragment>
+                <BlogTable
+                posts={this.state.posts}
+                post_userID={this.state.post_userID} />
+            </React.Fragment>
         <p style={{
           fontStyle: "italic"
         }}>
