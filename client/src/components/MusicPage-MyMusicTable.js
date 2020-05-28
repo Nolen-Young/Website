00671bc681453098dccacd7a787f0a171d046c74
nyoung1 <nolen.young@wsu.edu>
2020-05-28 09:45:01 -0700

    Added other music page connectivity. Music is now fully done.

--- client/src/components/MusicPage-MyMusicTable.js
+++ client/src/components/MusicPage-MyMusicTable.js
@@ -52,29 +52,3 @@ class MyMusicTable extends React.Component {
 }
 
 export default MyMusicTable;
-
-// Old table rendering logic for reference
-/* <div className = " music-players center ">
-				<table className = "music-table">
-					<thead className = " music-table-header ">
-						<tr className = "music-table-row">
-							<th className = "music-table-song-titles">Song Title</th>
-							<th className = "music-table-pseudonym">Released Under Pseudonym</th>
-							<th>Link</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr className = "music-table-row ">
-							<td className = "music-table-song-titles">Dark</td>
-							<td className = "music-table-pseudonym">South of Sound</td>
-							<td><ReactPlayer url = "https://soundcloud.com/southofsound/dark" width = "100%" height = "100px"/></td>
-						</tr>
-						<tr className = "music-table-row ">
-							<td className = "music-table-song-titles">Call Out My Name (DJ BEEF Remix)</td>
-							<td className = "music-table-pseudonym">DJ BEEF</td>
-							<td><ReactPlayer url = "https://soundcloud.com/user-243943999/the-weeknd-call-out-my-name-dj-beef-remix" width = "100%" height = "100px"/></td>
-						</tr>
-					</tbody>
-					<tfoot/>
-				</table>
-			</div> */
--- client/src/components/MusicPage-OtherMusic.js
+++ client/src/components/MusicPage-OtherMusic.js
@@ -1,90 +1,51 @@
 import React from "react";
-import ReactPlayer from "react-player";
+import axios from "axios";
+import OtherMusicTable from "./MusicPage-OtherMusicTable";
 
 // *****************************************************
 // This class contains the code to render the other music page.
 // *****************************************************
 class MusicPageOtherMusic extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.fetchOtherMusic = this.fetchOtherMusic.bind(this);
+
+		this.state = {
+			songs: [],
+		};
+	}
+
+	fetchOtherMusic = async () => {
+		axios
+			.get("http://localhost:5000/othermusic/")
+			.then((response) => {
+				if (response.data.length > 0) {
+					this.setState({
+						songs: response.data.map((song) => [
+							song.title,
+							song.artist,
+							song.link,
+						]),
+					});
+				}
+			})
+			.catch((err) => {
+				console.log("Error: " + err);
+			});
+	};
+
+	componentDidMount() {
+		this.fetchOtherMusic();
+	}
+
 	render() {
 		return (
-			<div>
-				<div>
+			<div id='otherMusicPage'>
+				<div id='header' className='center'>
 					<h1>Other People's Music I Like</h1>
 				</div>
-				<div className=' music-players center '>
-					<table className='music-table'>
-						<thead className=' music-table-header '>
-							<tr className='music-table-row'>
-								<th className='music-table-song-titles'>Song Title</th>
-								<th className='music-table-pseudonym'>Artist</th>
-								<th>Link</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr className='music-table-row '>
-								<td className='music-table-song-titles'>Doorman</td>
-								<td className='music-table-pseudonym'>Slowthai</td>
-								<td>
-									<ReactPlayer
-										url='https://soundcloud.com/slowthai/doorman?in=slowthai/sets/nothing-great-about-britain-2'
-										width='100%'
-										height='100px'
-									/>
-								</td>
-							</tr>
-							<tr className='music-table-row '>
-								<td className='music-table-song-titles'>8 (circle)</td>
-								<td className='music-table-pseudonym'>Bon Iver</td>
-								<td>
-									<ReactPlayer
-										url='https://soundcloud.com/boniver/8-circle'
-										width='100%'
-										height='100px'
-									/>
-								</td>
-							</tr>
-							<tr className='music-table-row '>
-								<td className='music-table-song-titles'>It Might Be Time</td>
-								<td className='music-table-pseudonym'>Tame Impala</td>
-								<td>
-									<ReactPlayer
-										url='https://soundcloud.com/tame-impala/it-might-be-time?in=tame-impala/sets/the-slow-rush'
-										width='100%'
-										height='100px'
-									/>
-								</td>
-							</tr>
-							<tr className='music-table-row '>
-								<td className='music-table-song-titles'>
-									Things It Would Have Been Helpful To Know Before The
-									Revolution
-								</td>
-								<td className='music-table-pseudonym'>Father John Mist</td>
-								<td>
-									<ReactPlayer
-										url='https://soundcloud.com/fatherjohnmisty/things-it-would-have-been-helpful-to-know-before-the-revolution'
-										width='100%'
-										height='100px'
-									/>
-								</td>
-							</tr>
-							<tr className='music-table-row '>
-								<td className='music-table-song-titles'>
-									Love It If We Made It
-								</td>
-								<td className='music-table-pseudonym'>The 1975</td>
-								<td>
-									<ReactPlayer
-										url='https://soundcloud.com/the1975/love-it-if-we-made-it?in=the1975/sets/a-brief-inquiry-into-online-1'
-										width='100%'
-										height='100px'
-									/>
-								</td>
-							</tr>
-						</tbody>
-						<tfoot />
-					</table>
-				</div>
+				<OtherMusicTable songs={this.state.songs} />
 			</div>
 		);
 	}
